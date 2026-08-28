8a4d2704e0c5c6085603d31fb947713b1c4a5fdb
Lawyer-rj <lawyer.rj@gmail.com>
2026-08-28 15:51:39 -0300

    Remove React.StrictMode - causes double rendering that creates flickering

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import { AuthProvider } from './contexts/AuthContext'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>,
 )
